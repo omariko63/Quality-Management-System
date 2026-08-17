c522e135ce99a8c3a48b9717ce121c92cbd89a01
kimmich4 <mohamed234552@bue.edu.eg>
2026-08-17 22:49:34 +0300

    Fixing errors

---
+++ src/main/java/com/example/quality_management_service/management/repository/ManagementRoleRepository.java
@@ -0,0 +1,11 @@
+package com.example.quality_management_service.management.repository;
+
+import com.example.quality_management_service.management.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ManagementRoleRepository extends JpaRepository<Role, Integer> {
+    boolean existsByRoleName(String roleName);
+    Optional<Role> findByRoleName(String roleName);
+}
